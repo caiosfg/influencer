b724888abd5117c39ac23d38bf2d31626d70a708
caiosfg <caio.sfg@gmail.com>
2025-05-16 18:12:23 -0300

    feat: apiresource route

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -11,4 +11,31 @@ class UserController extends Controller
         return User::all();
     }
 
+    public function show($id) {
+        return User::find($id);
+    }
+
+    public function store(Request $request) {
+
+        $user = User::create($request->all());
+
+        return response($user, 201);
+    }
+
+    public function update(Request $request, $id) {
+
+        $user = User::find($id);
+
+        $user->update($request->all());
+
+        return response($user, 202);
+    }
+
+    public function destroy($id) {
+
+        User::destroy($id);
+
+        return response(null, 204);
+    }
+
 }
