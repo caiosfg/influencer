1dbf992238ae82dbae6668858abfad5558b00ede
caiosfg <caio.sfg@gmail.com>
2025-05-22 17:35:20 -0300

    feat: passport and login

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,11 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 
 
+Route::post('login', [AuthController::class, 'login']);
+
 Route::apiResource('/users', UserController::class);
+
